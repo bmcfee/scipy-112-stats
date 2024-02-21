e08f225b8c1b4d2a5b9789e5ea9486805fd368ff
Brian McFee <brian.mcfee@nyu.edu>
2024-02-21 09:02:19 -0500

    adding reference data

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,10 +1,15 @@
 import numpy as np
 import scipy
+import os
+
+
 
 def test_stats_beta():
     thresholds = np.linspace(0, 1, 101)
     beta_parameters = (2, 18)
 
+    beta_cdf = np.load(os.path.join("tests", 'beta_cdf.npy'))
+
     outs = scipy.stats.beta.cdf(thresholds, beta_parameters[0], beta_parameters[1])
 
-    assert False, outs
+    assert np.allclose(outs, beta_cdf)
